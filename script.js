6b13daa33e0df81665b304aac474e01ea5152bca
Tyson Robles <tysonrobles33@gmail.com>
2024-08-23 10:32:50 -0500

    Add getComputerChoice() getsrock paper or scissors

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function test () {
-    console.log('success')
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random() * 3) + 1
+    if (computerChoice == 1) {
+        alert('Rock')
+    } else if (computerChoice == 2) {
+        alert('Paper')
+    } else if (computerChoice == 3) {
+        alert('Scissors') 
+    } else {
+        alert('Sorry, there was an error')
+    }
 }
