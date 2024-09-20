7fe53480e44677768bee797f9be8083fd5ce39bd
Tyson Robles <tysonrobles33@gmail.com>
2024-09-20 09:16:25 -0500

    Connected playRound() to the scoreboard so that each round increments round and score counters

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let playerScore = 0
-let computerScore = 0
+let playerScoreBox = document.querySelector('.playerScoreBox');
+let computerScoreBox = document.querySelector('.computerScoreBox');
+let roundCounterBox = document.querySelector('.roundCounterBox');
+let roundCounter = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
     choice = Math.floor(Math.random() * 3) + 1
     if (choice == 1) {
-        return 'rock'
+        return 'rock';
     } else if (choice == 2) {
-        return 'paper'
+        return 'paper';
     } else if (choice == 3) {
-        return 'scissors' 
+        return 'scissors' ;
     } else {
-        alert('Sorry, there was an error')
+        alert('Sorry, there was an error');
     }
 }
 
@@ -18,24 +22,44 @@ function getComputerChoice () {
 function playRound (playerChoice, computerChoice) {
     computerChoice = getComputerChoice()
     if (playerChoice == 'btn-rock' && computerChoice == 'scissors') {
-        playerScore++
-        console.log('You Win! Rock beats scissors.')
+        roundCounter++;
+        playerScore++;
+        roundCounterBox.textContent = roundCounter;
+        playerScoreBox.textContent = playerScore;
+            console.log('You Win! Rock beats scissors.');
     } else if (playerChoice == 'btn-paper' && computerChoice == 'rock') {
-        playerScore++
-        console.log('You Win! Paper beats rock.')
+        roundCounter++;
+        playerScore++;
+        roundCounterBox.textContent = roundCounter;
+        playerScoreBox.textContent = playerScore;
+            console.log('You Win! Paper beats rock.');
     } else if (playerChoice == 'btn-scissors' && computerChoice == 'paper') {
-        playerScore++
-        console.log('You Win! Scissors beats paper.')
+        roundCounter++;
+        playerScore++;
+        roundCounterBox.textContent = roundCounter;
+        playerScoreBox.textContent = playerScore;
+            console.log('You Win! Scissors beats paper.');
     } else if (playerChoice == 'btn-rock' && computerChoice == 'paper') {
-        computerScore++
-        console.log('You Lost. Paper beats rock')
+        roundCounter++;
+        computerScore++;
+        roundCounterBox.textContent = roundCounter;
+        computerScoreBox.textContent = computerScore;
+            console.log('You Lost. Paper beats rock');
     } else if (playerChoice == 'btn-paper' && computerChoice == 'scissors') {
-        computerScore++
-        console.log('You Lost. Scissors beats paper')
+        roundCounter++;
+        computerScore++;
+        roundCounterBox.textContent = roundCounter;
+        computerScoreBox.textContent = computerScore;
+            console.log('You Lost. Scissors beats paper');
     } else if (playerChoice == 'btn-scissors' && computerChoice == 'rock') {
-        computerScore++
-        console.log('You Lost. Rock beats scissors')
+        roundCounter++;
+        computerScore++;
+        roundCounterBox.textContent = roundCounter;
+        computerScoreBox.textContent = computerScore;
+            console.log('You Lost. Rock beats scissors');
     } else if (playerChoice == ("btn-" + computerChoice)){
+        roundCounter++;
+        roundCounterBox.textContent = roundCounter;
         console.log('Tie!')
     } else {
         console.log(playerChoice, computerChoice)
